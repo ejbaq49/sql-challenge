84bf043ad0fa8a4c3602a577b6d5842a05f6da93
EJBaquet <ejbaquet@yahoo.com>
2020-07-18 22:49:56 -0400

    add schema creation

--- EmployeeSQL/Pewlett_Hackard_employee_schema.sql
+++ EmployeeSQL/Pewlett_Hackard_employee_schema.sql
@@ -5,14 +5,20 @@
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title" int   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(25)   NOT NULL,
+    "last_name" varchar(25)   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "departments" (
-    "dept_no" int   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(25)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
@@ -20,26 +26,34 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
+    "dept_no" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" int   NOT NULL,
-    "emp_no" int   NOT NULL
+    "dept_no" varchar(10)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
 );
 
 CREATE TABLE "titles" (
     "title_id" varchar(10)   NOT NULL,
+    "title" varchar(25)   NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
